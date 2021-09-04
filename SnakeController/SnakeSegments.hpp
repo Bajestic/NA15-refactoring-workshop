5ec241ef3a14a67039d79edb2d669d7584fa482e
lukasz przybyla <lukasz.przybyla@hotmail.com>
2021-09-04 12:33:21 +0200

    add getter and setter to Controller class

---
+++ SnakeController/SnakeSegments.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <list>
+
+#include "SnakeInterface.hpp"
+#include "SnakePosition.hpp"
+
+namespace Snake
+{
+
+class Segments
+{
+public:
+    Segments(Direction direction);
+
+    bool isCollision(Position position) const;
+    void addSegment(Position position);
+    void addHead(Position position);
+    Position nextHead() const;
+    Position removeTail();
+    void updateDirection(Direction newDirection);
+    unsigned size() const;
+private:
+    Direction m_headDirection;
+    std::list<Position> m_segments;
+    inline static unsigned snakeLength{};
+};
+
+} // namespace Snake
